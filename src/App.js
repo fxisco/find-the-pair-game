2a8afa21ee4be4b7d5f5a1d9d14ccf9a0f218267
Francisco Payes <payesfrancisco@gmail.com>
2018-10-16 10:16:48 -0400

    feat: add logic to add selected class and remove it

--- src/App.js
+++ src/App.js
@@ -12,31 +12,68 @@ class App extends Component {
     super(props);
 
     this.state = {
-      level: 0
+      level: 0,
+      selected: []
     };
-  }
-  render() {
-    const { level } = this.state;
+
     const MAX = ICONS.length;
-    const qty = MIN_ELEMENTS + level * 2;
+    const qty = MIN_ELEMENTS + this.state.level * 2;
     const qtyPerLevel = qty > MAX ? MAX : qty;
     const copy = [...ICONS];
 
-    const iconsToShow = [...Array(qtyPerLevel)].map(() => {
+    const iconsSelected = [...Array(qtyPerLevel)].map(() => {
       const index = Math.floor((Math.random() * copy.length));
       const item = copy.splice(index, 1);
 
       return item[0];
     });
 
+    const iconsToShow = [...iconsSelected, ...iconsSelected];
+
+    this.state.iconsSortedRandomly = [...Array(qtyPerLevel * 2)].map(() => {
+      const index = Math.floor((Math.random() * iconsToShow.length));
+      const item = iconsToShow.splice(index, 1);
+
+      return item[0];
+    });
+
+    this.handleCardSelection = this.handleCardSelection.bind(this);
+  }
+
+  handleCardSelection(id) {
+    const { selected } = this.state;
+
+    if (selected.includes(id)) {
+      const index = selected.findIndex((item) => item === id);
+
+      this.setState({
+        ...this.state,
+        selected: [
+          ...selected.slice(0, index),
+          ...selected.slice(index + 1),
+        ]
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        selected: [
+          ...selected,
+          id
+        ]
+      });
+    }
+  }
+
+  render() {
     return (
       <div className="App">
         <div className="container">
           <div className="row">
-          {iconsToShow.map((item) => {
+          {this.state.iconsSortedRandomly.map((item, index) => {
+            const id = `card-${item}-${index}`;
             return (
-              <div key={`card-${item}`} className="col-sm-1 py-3">
-                <div className="card">
+              <div key={id} className="col-sm-1 py-3" onClick={this.handleCardSelection.bind(null, `card-${item}-${index}`)}>
+                <div className={`card ${this.state.selected.includes(id) ? 'selected' : '' }`}>
                   <div className="card-body">
                     <i className={`fa ${item}`} />
                   </div>
