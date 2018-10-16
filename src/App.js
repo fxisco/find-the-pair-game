02a1cc4cbb731e00ee9f67eae1313b0f33f73eb3
Francisco Payes <payesfrancisco@gmail.com>
2018-10-16 08:53:14 -0400

    Create font-awesome cards to play

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { ICONS } from './conf/index';
+
+const MIN_ELEMENTS = 10;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      level: 0
+    };
+  }
   render() {
+    const { level } = this.state;
+    const MAX = ICONS.length;
+    const qty = MIN_ELEMENTS + level * 2;
+    const qtyPerLevel = qty > MAX ? MAX : qty;
+    console.log(Math.floor((Math.random() * qtyPerLevel)));
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="row">
+          {ICONS.map((item) => {
+            return (
+              <div key={`card-${item}`} className="col-sm-1 py-3">
+                <div className="card">
+                  <div className="card-body">
+                    <i className={`fa ${item}`} />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          </div>
+        </div>
       </div>
     );
   }
