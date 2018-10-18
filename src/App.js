dcb11ca6373a2f9e8327ccfd7e04c3aa45a45113
Francisco Payes <payesfrancisco@gmail.com>
2018-10-18 09:09:41 -0400

    feat: Changing levels

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { ICONS } from './conf/index';
 
-const MIN_ELEMENTS = 10;
+const MIN_ELEMENTS = 2;
+const SELECTED_ITEMS_MAX = 2;
+const FLIP_TIMEOUT = 1000;
 
 class App extends Component {
   constructor(props) {
@@ -13,12 +13,37 @@ class App extends Component {
 
     this.state = {
       level: 0,
-      selected: []
+      selected: {},
+      paired: {},
+      iconsSortedRandomly: []
     };
 
-    const MAX = ICONS.length;
+    this.handleCardSelection = this.handleCardSelection.bind(this);
+    this.setGameLevel = this.setGameLevel.bind(this);
+  }
+
+  componentDidMount() {
+    this.setGameLevel();
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.level < this.state.level) {
+      this.setGameLevel();
+    }
+
+    if (Object.keys(this.state.paired).length === this.state.iconsSortedRandomly.length) {
+      this.setState({
+        level: this.state.level + 1,
+        selected: {},
+        paired: {}
+      });
+    }
+  }
+
+  setGameLevel() {
+    const max = ICONS.length;
     const qty = MIN_ELEMENTS + this.state.level * 2;
-    const qtyPerLevel = qty > MAX ? MAX : qty;
+    const qtyPerLevel = qty > max ? max : qty;
     const copy = [...ICONS];
 
     const iconsSelected = [...Array(qtyPerLevel)].map(() => {
@@ -30,57 +55,98 @@ class App extends Component {
 
     const iconsToShow = [...iconsSelected, ...iconsSelected];
 
-    this.state.iconsSortedRandomly = [...Array(qtyPerLevel * 2)].map(() => {
+    const iconsSortedRandomly = [...Array(qtyPerLevel * 2)].map(() => {
       const index = Math.floor((Math.random() * iconsToShow.length));
       const item = iconsToShow.splice(index, 1);
 
       return item[0];
     });
 
-    this.handleCardSelection = this.handleCardSelection.bind(this);
+    this.setState({
+      iconsSortedRandomly
+    });
   }
 
   handleCardSelection(id) {
     const { selected } = this.state;
 
-    if (selected.includes(id)) {
-      const index = selected.findIndex((item) => item === id);
-
+    if (!selected[id]) {
       this.setState({
         ...this.state,
-        selected: [
-          ...selected.slice(0, index),
-          ...selected.slice(index + 1),
-        ]
-      });
-    } else {
-      this.setState({
-        ...this.state,
-        selected: [
+        selected: {
           ...selected,
-          id
-        ]
+          [id]: id
+        }
+      }, () => {
+        if (Object.keys(this.state.selected).length === SELECTED_ITEMS_MAX) {
+          const matchingPairs = Object.keys(this.state.selected).reduce((accum, key) => {
+            const icon = key.split('_')[0];
+
+            if (!accum[icon]) {
+              accum[icon] = icon;
+            }
+
+            return accum;
+          }, {});
+
+          if (Object.keys(matchingPairs).length > 1) {
+            setTimeout(() => {
+              this.setState({
+                ...this.state,
+                selected: {}
+              });
+            }, FLIP_TIMEOUT);
+          } else {
+            setTimeout(() => {
+              this.setState({
+                ...this.state,
+                selected: {},
+                paired: {
+                  ...this.state.paired,
+                  ...this.state.selected
+                }
+              });
+            }, FLIP_TIMEOUT);
+          }
+        }
       });
     }
   }
 
   render() {
+    const { level } = this.state;
+
     return (
       <div className="App">
         <div className="container">
           <div className="row">
-          {this.state.iconsSortedRandomly.map((item, index) => {
-            const id = `card-${item}-${index}`;
-            return (
-              <div key={id} className="col-sm-1 py-3" onClick={this.handleCardSelection.bind(null, `card-${item}-${index}`)}>
-                <div className={`card ${this.state.selected.includes(id) ? 'selected' : '' }`}>
-                  <div className="card-body">
-                    <i className={`fa ${item}`} />
+            <h1>Find pairs!</h1>
+          </div>
+          <div className="row">
+            <h2 className="points-text">Level: {level + 1}</h2>
+          </div>
+          <div className="row">
+            <div className="grid">
+              {this.state.iconsSortedRandomly.map((item, index) => {
+                const id = `${item}_${index}`;
+
+                return (
+                  <div
+                    key={id}
+                    className={`flip-card ${this.state.selected[id] || this.state.paired[id] ? 'selected' : '' } ${this.state.paired[id] ? 'paired' : '' }`.trim()}
+                    onClick={ !this.state.paired[id] ? this.handleCardSelection.bind(null, id) : null}>
+                      <div className="card-body">
+                        <div className="card-back">
+                          <i className={`fa ${item}`.trim()} />
+                        </div>
+                        <div className="card-front">
+                          {index + 1}
+                        </div>
+                      </div>
                   </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
